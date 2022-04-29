46ed074ea8b4418af989a136cc89025511f58fcf
小野裕輔 <y-ono@nswt.co.jp>
2022-04-29 17:08:07 +0900

    Add Template Method パターン

---
+++ study-template-method-pattern/src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        AbstractProcess process1 = new CharDisplay('H');
+        process1.execute();
+
+        System.out.println();
+
+        AbstractProcess process2 = new StringDisplay("Hello");
+        process2.execute();
+    }
+}
